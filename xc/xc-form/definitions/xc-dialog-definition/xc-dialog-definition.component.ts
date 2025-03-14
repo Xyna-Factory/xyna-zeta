94df5ca018ab23946a47f995f76cf4e4a96cfc43
Torben Siegismund <127193495+TorbenSiegismund-GIP@users.noreply.github.com>
2025-03-14 11:43:40 +0100

    show errormessage, if show result is true (#252)

--- xc/xc-form/definitions/xc-dialog-definition/xc-dialog-definition.component.ts
+++ xc/xc-form/definitions/xc-dialog-definition/xc-dialog-definition.component.ts
@@ -93,12 +93,21 @@ export class XcDialogDefinitionComponent extends XcDialogComponent<Xo[], XoDefin
         }
 
         const rtc = (definition.serviceRTC ? definition.serviceRTC : this.getDefaultRTC()).toRuntimeContext();
-        return preStartorder.pipe(switchMap(() => this.api.startOrder(
-            rtc,
-            definition.serviceFQN,
-            input, null,
-            new StartOrderOptionsBuilder().withErrorMessage(true).async(!definition.synchronously).options
-        )), map(result => result.output));
+        return preStartorder.pipe(
+            switchMap(() =>
+                this.api.startOrder(rtc, definition.serviceFQN, input, null,
+                    new StartOrderOptionsBuilder().withErrorMessage(true).async(!definition.synchronously).options)
+            ),
+            filter(result => {
+                if (!result || result.errorMessage) {
+                    if (definition.showResult) {
+                        this.dialogs.error(result.errorMessage);
+                    }
+                    return false;
+                }
+                return true;
+            }),
+            map(result => result.output));
     }
 
 
