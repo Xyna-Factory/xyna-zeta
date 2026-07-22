6ca1df23577fa98a7127e0d68cf3e7898496c007
Daniel Schäfer <138758294+DanielSchaefer-telekom@users.noreply.github.com>
2026-07-22 11:14:35 +0200

    add role list dropdown to smartcard login (#445)
    * add dropdown for multiple roles in jwt roles claim
    
    needs backend with modifications https://github.com/DanielSchaefer-telekom/xyna-factory/tree/JWT_10.8_with_roleListForDropdown
    
    * update to use multiple domains
    
    ---------
    
    Co-authored-by: PatrickVonMassow-GIP <111737360+PatrickVonMassow-GIP@users.noreply.github.com>

--- auth/login/auth-login.component.ts
+++ auth/login/auth-login.component.ts
@@ -24,6 +24,8 @@ import { environment } from '@environments/environment';
 import { XcI18nContextDirective, XcI18nTranslateDirective } from '../../i18n/i18n.directive';
 import { I18nParam, I18nService } from '../../i18n/i18n.service';
 import { XcDialogService, XcTabBarItem } from '../../xc';
+import { XcAutocompleteDataWrapper } from '../../xc/xc-form/xc-form-autocomplete/xc-form-autocomplete.component';
+import { XcOptionItemString } from '../../xc/shared/xc-item';
 import { XcButtonComponent } from '../../xc/xc-button/xc-button.component';
 import { XcIconComponent } from '../../xc/xc-icon/xc-icon.component';
 import { XcLanguageSelectorComponent } from '../../xc/xc-language-selector/xc-language-selector.component';
@@ -42,10 +44,14 @@ import { WorkflowLoginComponent } from '../forms/workflow-login.component';
 export interface LoginComponentData {
     username: string;
     password?: string;
+    selectedRole?: string;
+    roleSelectionDataWrapper?: XcAutocompleteDataWrapper<string>;
     disabled?: boolean;
     onEnter: () => void;
     usernameTabIndex: number;
     usernameSuffixTabIndex: number;
+    roleTabIndex?: number;
+    roleSuffixTabIndex?: number;
     passwordTabIndex?: number;
     passwordSuffixTabIndex?: number;
 }
@@ -71,15 +77,24 @@ export class AuthLoginComponent {
     protected readonly i18n = inject(I18nService);
 
 
+    private readonly smartCardRoleDataWrapper = new XcAutocompleteDataWrapper<string>(
+        () => this.smartCardTabItem.data.selectedRole,
+        (role: string) => this.smartCardTabItem.data.selectedRole = role
+    );
+
     readonly smartCardTabItem: LoginTabItem = {
         closable: false,
         component: SmartCardLoginTabComponent,
         name: 'SmartCard',
         data: <LoginComponentData>{
             username: '',
+            selectedRole: undefined,
+            roleSelectionDataWrapper: this.smartCardRoleDataWrapper,
             onEnter: this.login.bind(this),
             usernameTabIndex: 1,
             usernameSuffixTabIndex: 4,
+            roleTabIndex: 2,
+            roleSuffixTabIndex: 5,
         }
     };
 
@@ -214,7 +229,24 @@ export class AuthLoginComponent {
             filter(info => !!info)
         ).subscribe(info => {
             this.smartCardTabItem.data.username = info.username || '';
-            this.smartCardDomain = (info.externaldomains || [])[0] || '';
+            const externalDomains = (info.externaldomains || []).filter(domain => !!domain);
+            const domainNames = externalDomains.length > 0
+                ? externalDomains
+                : (info.domains || []).map(domain => domain.name).filter(name => !!name);
+
+            this.smartCardDomain = domainNames[0] || '';
+
+            const domainFromResponse = (info.domains || []).find(domain => domain.name === this.smartCardDomain)
+                || (info.domains || [])[0];
+
+            const roles = (domainFromResponse?.roles || []).filter(role => !!role);
+
+            if (roles.length === 0) {
+                this.smartCardTabItem.data.selectedRole = undefined;
+            } else if (!this.smartCardTabItem.data.selectedRole || !roles.includes(this.smartCardTabItem.data.selectedRole)) {
+                this.smartCardTabItem.data.selectedRole = roles[0];
+            }
+            this.smartCardRoleDataWrapper.values = roles.map(role => XcOptionItemString(role));
         });
     }
 
@@ -231,7 +263,7 @@ export class AuthLoginComponent {
     smartCardLogin(force = false) {
         this.pending = true;
         const forcedLogin = force || !!(environment.zeta.auth && environment.zeta.auth.useTheForcedLogin);
-        this.authService.smartCardLogin(this.smartCardDomain, forcedLogin).pipe(
+        this.authService.smartCardLogin(this.smartCardDomain, forcedLogin, this.smartCardTabItem.data.selectedRole).pipe(
             catchError(error => {
                 /**
                  * TODO
