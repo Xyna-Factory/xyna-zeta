84986473c46137ca8bfc29f6b1f44e56f1e4b286
jvs-gip <julian.vonselasinsky@gip.com>
2026-08-26 14:17:11 +0200

    xc-has-right

--- xc/shared/xc-has-right.directive.ts
+++ xc/shared/xc-has-right.directive.ts
@@ -15,7 +15,7 @@
  * limitations under the License.
  * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
  */
-import { Directive, inject, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+import { Directive, effect, inject, input, TemplateRef, ViewContainerRef } from '@angular/core';
 
 import { AuthService } from '../../auth/auth.service';
 import { isString } from '../../base';
@@ -26,24 +26,20 @@ export class XcHasRightDirective {
     private readonly authService = inject(AuthService);
     private readonly viewContainerRef = inject(ViewContainerRef);
     private readonly templateRef = inject<TemplateRef<any>>(TemplateRef);
+    readonly neededRightsInput = input('', { alias: 'xc-has-right' });
 
 
+    constructor() {
+        effect(() => {
+            const rightsArray = (isString(this.neededRightsInput()) ? this.neededRightsInput().split(',') : [])
+                .map(item => item.trim())
+                .filter(right => right);
 
-    @Input('xc-has-right')
-    set neededRights(value: string) {
-
-        const rightsArray = (isString(value) ? value.split(',') : [])
-            .map(item => item.trim()) // trims trailing whitespaces
-            .filter(right => right);  // filters out empty elements
-
-        const hide = rightsArray.some(
-            right => !this.authService.hasRight(right)
-        );
-
-        if (hide) {
+            const hide = rightsArray.some(right => !this.authService.hasRight(right));
             this.viewContainerRef.clear();
-        } else {
-            this.viewContainerRef.createEmbeddedView(this.templateRef);
-        }
+            if (!hide) {
+                this.viewContainerRef.createEmbeddedView(this.templateRef);
+            }
+        });
     }
 }
